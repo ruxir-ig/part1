dea5350212ac6396e7b1e425ad3fddceb81045cd
Ruchir Kalokhe <ruchirkalokhe@gmail.com>
2025-08-14 17:48:59 +0530

    Ex 1.2 done

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -8,18 +8,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return(
     <>
-    <p>
-      {props.parts.part1} {props.exercises.exercises1}
-    </p>
-    <p>
-      {props.parts.part2} {props.exercises.exercises2}
-    </p>
-    <p>
-      {props.parts.part3} {props.exercises.exercises3}
-    </p>
+    <Part part = {props.parts.part1} exercises = {props.exercises.exercises1}/>
+    <Part part = {props.parts.part2} exercises = {props.exercises.exercises2}/>
+    <Part part = {props.parts.part3} exercises = {props.exercises.exercises3}/>
     </>
   )
 }
